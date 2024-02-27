da85303b9cdba51f5480dfd4ed984e6fba17714c
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-02-27 13:43:30 +0600

    added stylization in item component

--- src/container/todoItem.tsx
+++ src/container/todoItem.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { toggleTodo } from "./todoSlice"
 
-const TodoItem: React.FC = () => {
-  return (
-    <div>
-      <div className="border-2 border-amber-50 w-40 h-10 text-center rounded-md mt-10">
-        <p className="mt-1.5">
-          todos
-        </p>
-      </div>
-    </div>
-  );
+const TodoItem: React.FC<{ id: string; title: string; completed: boolean }> = ({ id, title, completed }) => {
+    const dispatch = useDispatch();
+
+    const handleToggle = () => {
+        dispatch(toggleTodo(id));
+    };
+
+    return (
+        <div>
+            <div className="border-2 border-amber-50 w-40 h-10 text-center rounded-md mt-10 flex justify-between items-center">
+                <p className="ml-5">{title}</p>
+                <input type="checkbox" checked={completed} onChange={handleToggle} className="mr-5" />
+            </div>
+        </div>
+    );
 };
 
 export default TodoItem;
