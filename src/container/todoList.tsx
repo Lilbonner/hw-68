01a629e871c428f9523a6e46cd78d5a4187877ce
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-02-27 13:44:51 +0600

    added stylization in todo list component

--- src/container/todoList.tsx
+++ src/container/todoList.tsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { RootState } from '../App/Store';
+import { fetchTodos } from './todoSlice';
 
 const TodoList: React.FC = () => {
+  const dispatch = useDispatch();
+  const todos = useSelector((state: RootState) => state.todos.todos);
+
+  useEffect(() => {
+    dispatch(fetchTodos());
+  }, []);
+
   return (
-    <div>
-      <div>
-        <input placeholder="add todos"/>
-        <button>save</button>
+      <div className="bg-emerald-800 h-20">
+        <div className="text-2xl">
+          <input className="ml-5 mt-7 rounded-md bg-emerald-50 text-black hover:border-black" placeholder="add todos:" />
+          <button className="ml-4 border bg-emerald-50 text-emerald-700 w-20 hover:bg-emerald-100">add</button>
+        </div>
+        <ul>
+          {todos.map((todo) => (
+              <li key={todo.id}>
+                {todo.title}
+              </li>
+          ))}
+        </ul>
       </div>
-    </div>
   );
 };
 
